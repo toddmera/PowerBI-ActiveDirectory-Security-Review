864b8f6a9e71bda44467a92940ab08627c19585e
toddmera <22841338+toddmera@users.noreply.github.com>
2019-10-01 09:54:43 -0700

    Update ADSecurityInfo.ps1

--- ADSecurityInfo.ps1
+++ ADSecurityInfo.ps1
@@ -1,4 +1,4 @@
-﻿###############################################
+###############################################
 # Script to collect Windows Server information, 
 # Services installed on the servers and user information.
 #
@@ -6,23 +6,34 @@
 #
 ############################################### 
 
-#### Start Edit ####
 
+# Set the path where csv files will be stored.  Must be full path excluding final "\"
 $myPath = "C:\Temp"
 
-#### End Edit ######
+# Get information about the domian
+Get-ADDomain | Select-Object -Property DNSRoot, NetBIOSName, DomainMode | Export-Csv -Path $myPath\domain.csv -NoTypeInformation
 
+# Get list of users in AD
+Get-ADUser -Filter * -Properties Enabled, LastLogonDate, PasswordLastSet,PasswordNeverExpires, SIDHistory, TrustedForDelegation, TrustedToAuthForDelegation, userAccountControl | Export-Csv -Path $myPath\users.csv -NoTypeInformation
 
-Get-ADComputer -Filter {(OperatingSystem -like "*Windows Server*")} -Properties * | Export-Csv -Path $myPath\servers.csv -NoTypeInformation
-Get-ADUser -Filter * | Export-Csv -Path $myPath\users.csv -NoTypeInformation
-
-$computers = Get-ADComputer -Filter {(OperatingSystem -like "*Windows Server*")}
+# Get servers in AD as well as any service running on the server.
+$computers = Get-ADComputer -Filter {(OperatingSystem -like "*Windows Server*")}  -Properties LastLogonDate,OperatingSystem, PasswordLastSet, primaryGroupID, TrustedForDelegation, TrustedToAuthForDelegation
+$computers | Export-Csv -Path $myPath\servers.csv -NoTypeInformation
 foreach($Computer in $computers)
     {
-        Get-WmiObject win32_service -ComputerName $Computer.DNSHostName | Export-Csv -Path $myPAth\services.csv -NoTypeInformation -Append
+        Get-WmiObject win32_service -ComputerName $Computer.DNSHostName -ErrorAction SilentlyContinue | Select-Object -Property DisplayName, Name, PathName, PSComputerName, startName, startMode, State | Export-Csv -Path $myPAth\services.csv -NoTypeInformation -Append -Force
 
     }
 
 
 
-
+##########################################
+## Useful info
+#
+### PrimaryGroupID
+# 515 – Domain Computers
+# 516 – Domain Controllers (writable)
+# 521 – Domain Controllers (Read-Only)
+####
+#
+##########################################
